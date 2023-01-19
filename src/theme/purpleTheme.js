b84f50ed1d27d7249d4d11013adc72a3125053ec
Bárbara B <bbeatriz@gmail.com>
2023-01-19 12:01:24 +0100

    login layout and register created

--- src/theme/purpleTheme.js
+++ src/theme/purpleTheme.js
@@ -3,7 +3,7 @@ import { red } from "@mui/material/colors";
 
 export const purpleTheme = createTheme({
     palette: {
-        prmary: {
+        primary: {
             main: '#262254'
         },
         secondary: {
