8f773305be774653da84d48808507b21d6a5c702
Bárbara B <bbeatriz@gmail.com>
2023-01-25 11:10:00 +0100

    layout and design completed

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,30 @@
-import { Typography } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-
+import { NoteView, NothingSelectedView } from "../views";
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-        <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque inventore voluptates qui sequi suscipit maiores quibusdam consequuntur distinctio mollitia, autem nostrum consequatur? Similique consectetur at rem maxime corrupti velit dolorum!</Typography>
+      {/* <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque inventore voluptates qui sequi suscipit maiores quibusdam consequuntur distinctio mollitia, autem nostrum consequatur? Similique consectetur at rem maxime corrupti velit dolorum!</Typography> */}
 
-        {/* {NothingSelected} */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
 
     </JournalLayout>
   )
