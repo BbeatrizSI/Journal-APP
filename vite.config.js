fd282aec93bf30760f493d92c39b46ba47331645
BbeatrizSI <bbeatriz@gmail.com>
2025-02-19 14:02:49 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  // base: '/Journal-APP/',
+  // base: "./",
+  base: '/Journal-APP/',
   plugins: [react()]
 })
