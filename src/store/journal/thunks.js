4e1d1a9158f6a5aa2af4725c52bbee6e80fae36a
BbeatrizSI <bbeatriz@gmail.com>
2025-03-04 12:11:22 +0100

    delete note

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,6 @@
-import { collection, setDoc, doc } from "firebase/firestore/lite";
+import { collection, setDoc, doc, deleteDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes, setSaving, updateNote, setPhotosToActiveNote } from "./journalSlice";
+import { addNewEmptyNote, deleteNoteById, setActiveNote, savingNewNote, setNotes, setSaving, updateNote, setPhotosToActiveNote } from "./journalSlice";
 import { loadNotes } from "../../helpers/loadNotes";
 import { fileUpload } from "../../helpers/fileUpload";
 
@@ -14,6 +14,7 @@ export const startNewNote = () => {
         const newNote = {
             title: '',
             body: '',
+            imageUrls: [],
             date: new Date().getTime(),
         }
 
@@ -66,6 +67,20 @@ export const startUploadingFiles = ( files = [] ) => {
         const photosUrls = await Promise.all( fileUploadPromises );
 
         dispatch( setPhotosToActiveNote( photosUrls ) );
+        
+    }
+}
+
+export const startDeletingNote = () => {
+    return async( dispatch, getState ) => {
+
+        const { uid } = getState().auth;
+        const { active:note } = getState().journal;
+
+        const docRef = doc( FirebaseDB, `${ uid }/journal/notes/${ note.id }` );   
+        await deleteDoc( docRef );
+
+        dispatch( deleteNoteById( note.id ) );
         
     }
 }
